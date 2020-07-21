63f47cd51ae376fa6ffd756d70d7a94717386527
Felts <felts.tj@pg.com>
2020-07-21 16:06:56 -0400

    creating package

---
+++ R/sum2.R
@@ -0,0 +1,14 @@
+#' Function for summing 2 numbers
+#'
+#' @import base
+#' @importFrom base agrep
+#' @param num1 First number to sum. Default is 3.
+#' @param num2 Second number to sum. Default is 4.
+#' @return Sum of num1 and num2
+#' @export
+#' @details Function for summing two numbers
+#' @examples sum2(num1=3, num2=5)
+
+sum2 <- function(num1=3, num2=4) {
+  num1 + num2
+}
